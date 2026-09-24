d38de16227dc3ac6d2eb2eec853a50b4399feae2
Sanapathi Revathi <revathisanapathi742@gmail.com>
2026-09-24 12:24:49 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,38 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #data type
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> b="revathi"
+>>> type(b)
+<class 'str'>
+>>> c='revathi'
+>>> type(c)
+<class 'str'>
+>>> d='''revathi'''
+>>> type(d)
+<class 'str'>
+>>> e=True
+>>> type(e)
+<class 'bool'>
+>>> f=False
+>>> type(f)
+<class 'bool'>
+>>> g=2+9j
+>>> type(g)
+<class 'complex'>
+>>> h=2j
+>>> type(h)
+<class 'complex'>
+>>> i=0.8
+>>> type(i)
+<class 'float'>
+>>> k=true
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    k=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+>>> k="n"
+>>> type(k)
+<class 'str'>
